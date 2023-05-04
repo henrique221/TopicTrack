f6ddf3bcc02266ad5f1a4f9d71c042fdf7421663
Henrique Silva <hborgesdasilva9294@gmail.com>
2023-05-04 19:24:44 -0300

    feat(TopicTrack): update summarization

--- topictrack/summarize.py
+++ topictrack/summarize.py
@@ -9,26 +9,37 @@ import subprocess
 from dotenv import load_dotenv
 from rich import print
 from rich.console import Console
+from tqdm import tqdm
 
 console = Console()
 
 load_dotenv()
 openai_api_key = os.getenv('OPENAI_API_KEY')  # Insert your API key here
 
-def split_text(text, n=800):
+def split_text(text, n=1000):
     """Splits the text into smaller parts with at most n words."""
     words = text.split()
     return [" ".join(words[i : i + n]) for i in range(0, len(words), n)]
 
-def summarize_text(text, title, api_key):
+def summarize_text(file_path):
+    with open(file_path, 'r') as f:
+        text = f.read()
+
+    title = console.input('[bold]Title of the video (optional):[/bold] ')
+
+    total_words = len(text)
+    print(f"Quantidade de palavras: {total_words}")
+
+    if(total_words > 3900):
+        text = split_transcription(text, title, openai_api_key)
     """Generates a summary of the text using the OpenAI API."""
-    openai.api_key = api_key
+    openai.api_key = openai_api_key
 
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=f"Titulo:{title}\n\n{text}\n\nFaça um resumo detalhado em português brasileiro sobre essa transcrição, cite alguns tópicos importantes e explique cada um.",
         temperature=0.3,
-        max_tokens=500,
+        max_tokens=1000,
     )
     return response.choices[0].text.strip()
 
@@ -40,7 +51,7 @@ def summarize_text_part(text, api_key):
         engine="text-davinci-003",
         prompt=f"{text}\n\nFaça um resumo.",
         temperature=0.3,
-        max_tokens=500,
+        max_tokens=1000,
     )
     return response.choices[0].text.strip()
 
@@ -53,7 +64,7 @@ def continue_summary(text, api_key):
         engine="text-davinci-003",
         prompt=f"{text}\n\nContinue",
         temperature=0.3,
-        max_tokens=500,
+        max_tokens=1000,
     )
     return response.choices[0].text.strip()
 
@@ -64,12 +75,11 @@ def split_transcription(text, title, api_key):
     summaries = []
     total_words = 0
 
-    for remaining, chunk in enumerate(chunks, start=1):
-        print(f"Please wait while we are summarizing the text... {remaining} parts remaining")
+    console.print("Please wait while we are summarizing the text...")
+    for chunk in tqdm(enumerate(chunks, start=1), total=len(chunks), desc="Summarizing"):
         summary = summarize_text_part(chunk, api_key)
         total_words += len(summary.split())
         summaries.append(summary)
-
         if total_words > 3500:
             break
 
@@ -136,30 +146,65 @@ def finish(summarized_transcription_json_formatted):
     elif end == "p":
         main()
 
+def list_txt_files(path):
+    txt_files = []
+    for root, _, files in os.walk(path):
+        for file in files:
+            if file.endswith('.txt'):
+                txt_files.append(os.path.join(root, file))
+    return txt_files
+
+def choose_text_file():
+    """Choose a text file from the data directory and its subdirectories."""
+    media_path = 'data'
+
+    if not os.path.exists(media_path):
+        print(f"The directory '{media_path}' does not exist.")
+        return
+
+    files = list_txt_files(media_path)
+    if not files:
+        print(f"\nThere are no text files in the '{media_path}' directory or its subdirectories.\n[cyan]Please add your text files first[/cyan]\n")
+        return
+
+    print("Available files:")
+    for i, f in enumerate(files):
+        print(f"{i + 1}. {f}")
+
+    file_choice = int(input("\nChoose a file by number: ")) - 1
+    chosen_file = files[file_choice]
+
+    return chosen_file
+
+def first_choice():
+    option = console.input('\n\n[bold][cyan]Do you wish to[/cyan]\n\ntranscript a media (M)\nsummarize a text (T)\n[/bold]').lower()
+    if(option != 't' and option != 'm'):
+        return
+    return option
+
 def main():
     console.print("\n[bold green]Welcome to the TopicTrack Video Summarizer![/bold green]")
     console.print("[cyan]Please enter the following information:[/cyan]")
 
-    video_path = choose_video_file()
-    if(not video_path):
-        return
+    option = first_choice()
 
-    file_path = run_whisper_command(video_path)
+    if(not option):
+        console.print(f"\n[bold red]You must choose an option[/bold red]\n")
+        main()
+    elif(option == 'm'):
+        video_path = choose_video_file()
+        if(not video_path):
+            return
 
-    console.print(f"\n[bold cyan]Filepath:[/bold cyan]\n[magenta]{file_path}[/magenta]\n")
+        file_path = run_whisper_command(video_path)
 
-    title = console.input('[bold]Title of the video (optional):[/bold] ')
+        console.print(f"\n[bold cyan]Filepath:[/bold cyan]\n[magenta]{file_path}[/magenta]\n")
 
-    with open(file_path, 'r') as f:
-        text = f.read()
 
-    total_words = len(text)
-    print(f"Quantidade de palavras: {total_words}")
+    elif(option == 't'):
+        file_path = choose_text_file()
 
-    if(total_words > 3900):
-        text = split_transcription(text, title, openai_api_key)
-
-    summarized_transcription = summarize_text(text, title, openai_api_key)
+    summarized_transcription = summarize_text(file_path)
 
     pattern = re.compile(r'\n(\d?)')
     summarized_transcription_json_formatted = pattern.sub(replace_newline, summarized_transcription)
@@ -167,6 +212,7 @@ def main():
     console.print(f"\n[bold magenta]Result:[/bold magenta]\n\n{summarized_transcription_json_formatted}\n")
 
     finish(summarized_transcription_json_formatted)
+
 
 if __name__ == "__main__":
     main()
